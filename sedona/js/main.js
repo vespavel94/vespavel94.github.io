c63528d9c95d6a3709ffc5601449c565ebf01043
Pavel Veselov <ves.pavel94@gmail.com>
2017-12-07 04:15:39 +0300

    hover, active, logo

--- sedona/js/main.js
+++ sedona/js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
     $(".hotel-search__button").on("click", function () {
         $(this).toggleClass("button--disabled");
-    })
+    });
+
+    $(".html_academy_copyright__link").mouseover(function () {
+        $(".html_academy_copyright__logo").attr("src", "img/vector-smart-object-copy-8.png");
+    }).mouseout(function () {
+        $(".html_academy_copyright__logo").attr("src", "img/htmlacademy-svg.png");
+    });
 });
