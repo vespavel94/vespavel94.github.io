09af05e8f3889789cbd9f70ac37fae3a9ee32973
pavel <ves.pavel94@gmail.com>
2017-12-27 16:49:33 +0300

    datepicker is ready

--- sedona/js/datepicker.js
+++ sedona/js/datepicker.js
@@ -3,6 +3,7 @@ var trigger = $(".date-input input");
 const DAYS_IN_MONTH = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 var date = new Date();
 var currDay = date.getDate();
+var constMonth = date.getMonth();
 var options = {
     month: 'long'
 };
@@ -10,7 +11,6 @@ var options = {
 trigger.on("click", renderDatepicker);
 
 function renderDatepicker() {
-    console.log($(this));
     $(document).on("mouseup", removeDatepicker);
 
     var calendarWrapper = $(document.createElement("div"));
@@ -31,12 +31,49 @@ function renderDatepicker() {
         "  </div>");
     var dateWrapper = $(document.createElement("div"));
     dateWrapper.addClass("cal-container__date-wrapper");
+    renderDate(dateWrapper);
+    $(".day_cell").on("click", insertDate);
+
+    dateWrapper.appendTo($(calendarWrapper));
+    calendarWrapper.appendTo($(this).closest(".date-input"));
+
+
+    $(".day_cell").on("click", insertDate);
+    $(".next").on("click", function () {
+        date.setMonth(date.getMonth() + 1);
+        dateWrapper.empty();
+        renderDate(dateWrapper);
+
+    });
+    $(".prev").on("click", function () {
+        date.setMonth(date.getMonth() - 1);
+        dateWrapper.empty();
+        renderDate(dateWrapper);
+    });
+}
+
+
+function insertDate() {
+    $(this).parents(".date-input").children("input").val($(this).html() + "  " + date.toLocaleString("ru", options) + "  " + date.getFullYear());
+    $(".cal-container").remove();
+}
+
+function removeDatepicker(event) {
+    if ($(".cal-container").has(event.target).length === 0) {
+        $(".cal-container").remove();
+        $(document).unbind("mouseup");
+    }
+}
+
+function renderDate(dateWrapper) {
+    $(".curr-month-year").html(date.toLocaleString("ru", options) + " " + date.getFullYear());
     if (date.getFullYear() % 4 == 0) {
         DAYS_IN_MONTH[1] = 29;//visokosnii god
     }
 
     var currMonth = date.getMonth();
     date.setDate(1);
+
     var startDayOfMonth = date.getDay() - 1;
     var daysCount = DAYS_IN_MONTH[currMonth];
     for (var i = 0; i < startDayOfMonth; i++) {
@@ -48,36 +85,10 @@ function renderDatepicker() {
         var cell = $(document.createElement("div"));
         cell.addClass("day_cell");
         cell.html(i);
-        if (i == currDay) {
+        if (i == currDay && currMonth == constMonth) {
             cell.addClass("day--current");
         }
         cell.appendTo(dateWrapper);
     }
-
-    dateWrapper.appendTo($(calendarWrapper));
-    console.log($(this));
-    console.log($(this).closest(".date-input"));
-    calendarWrapper.appendTo($(this).closest(".date-input"));
-
-
     $(".day_cell").on("click", insertDate);
-    $(".next").on("click", function () {
-        console.log($(this));
-        $(".cal-container").remove();
-        date.setMonth(date.getMonth() + 1);
-        console.log($(this));
-        renderDatepicker.call($(this));
-    })
 }
-
-function insertDate() {
-    $(this).parents(".date-input").children("input").val($(this).html() + "  " + date.toLocaleString("ru", options) + "  " + date.getFullYear());
-}
-
-function removeDatepicker() {
-    if ($(".cal-container").has(event.target).length === 0) {
-        $(".cal-container").remove();
-        $(document).unbind("mouseup");
-    }
-}
-
